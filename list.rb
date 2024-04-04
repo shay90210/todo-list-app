062b21882319e74fcfd09f60ef77cf9eb90dfc4c
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-04 15:41:50 +0000

    added loop do and case statements

--- list.rb
+++ list.rb
@@ -14,13 +14,29 @@ class List
     self.name = gets.chomp.capitalize
     puts "\n"
 
-    puts "Hello #{self.name}! What would you like to do today?"
+    loop do
+      puts "Hello #{self.name}! What would you like to do today?"
+      puts "\n"
+      puts "1. Add a note"
+      puts "2. Access your list"
+      puts "3. Modify a note"
+      puts "4. Delete a note"
+
+      choice = gets.chomp.to_i
+      case choice
+      when 1
+      when 2
+      when 3
+      when 4
+      else
+      end
+    end
   end
 
   def add_note
   end
 
-  def access_note
+  def access_list
   end
 
   def modify_note
