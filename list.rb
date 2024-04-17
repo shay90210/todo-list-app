2382f765a78b15b68befec5e103cf381713cf72f
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-17 01:21:28 +0000

    added replacement code

--- list.rb
+++ list.rb
@@ -88,6 +88,9 @@ class List
       puts "\n"
       puts "What would you like to enter instead?"
       new_note = gets.chomp
+      @notes.delete_at(note_to_replace - 1)
+      @notes.push(new_note)
+      puts "#{note_to_replace} was replaced with #{new_note}."
     else
       puts "\n"
       puts "Note is not found on list."
