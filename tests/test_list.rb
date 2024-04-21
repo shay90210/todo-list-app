29d6fea3b35cde4a44f7c207932fec98e95f0cd1
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-21 02:39:37 +0000

    added assert equal and failed message

--- tests/test_list.rb
+++ tests/test_list.rb
@@ -6,5 +6,6 @@ require './list.rb'
 class TestList < Minitest::Test 
   def test_add_note
     list = List.new
+    assert_equal "'#{user_note}' has been added to #{self.name}'s list!", "Add Note Method Failed"
   end
 end
