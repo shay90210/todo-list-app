16fa29819f6d63c72e78554d132ed077bfdd58a4
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-23 15:27:35 +0000

    updated names in minitest and added missing end

--- tests/test_list.rb
+++ tests/test_list.rb
@@ -3,23 +3,26 @@
 require 'minitest/autorun'
 require './list.rb'
 
-class MyInputProcessor
+class AddNoteProcessor
   def get_user_input
     print "What would you like to add to your list?"
     gets.chomp
   end
 
   def store_input
-    input_array = []
-    input = get_user_input()
-    input_array << input
-    return input_array
+    @notes = []
+    user_note = get_user_input()
+    @notes << user_note
+    return @notes
   end
-end
 
-class TestList < Minitest::Test 
-  def test_add_note
-    @list = List.new
-    assert_equal("'cooking dinner' has been added to Shannon's list!", @list.notes)
+  class TestList < Minitest::Test 
+    def test_add_note
+      processor = AddNoteProcessor.new
+      
+      processor.stub :get_user_input, "test user_note" do
+        assert_equal ["test user_note"], processor.store_input()
+      end
+    end
   end
 end
