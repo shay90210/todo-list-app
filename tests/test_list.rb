cf92eb839a5615e6966597cbdf25dd2b21b27e95
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-23 15:01:33 +0000

    updated minitest

--- tests/test_list.rb
+++ tests/test_list.rb
@@ -6,6 +6,6 @@ require './list.rb'
 class TestList < Minitest::Test 
   def test_add_note
     @list = List.new
-    assert_equal("'#{user_note}' has been added to Shannon's list!", @list.notes)
+    assert_equal("'cooking dinner' has been added to Shannon's list!", @list.notes)
   end
 end
