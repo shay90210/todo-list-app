2656d7489210561a9fa363056a44cb981a1e52c9
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-23 14:17:10 +0000

    updated minitest to have list with @ and changed @list.add_note to @list.notes

--- tests/test_list.rb
+++ tests/test_list.rb
@@ -5,7 +5,7 @@ require './list.rb'
 
 class TestList < Minitest::Test 
   def test_add_note
-    list = List.new
-    assert_equal("'cooking dinner' has been added to Shannon's list!", @list.add_note)
+    @list = List.new
+    assert_equal("'cooking dinner' has been added to Shannon's list!", @list.notes)
   end
 end
