d1929e0d5b64902b44e7b0849cc0e7d4a56ed778
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-27 18:18:26 +0000

    renamed test_list.rb to test_add to indicate that the file will only check for adding a note into the list

---
+++ tests/test_add.rb
@@ -0,0 +1,28 @@
+# tests/test_list.rb
+
+require 'minitest/autorun'
+require './list.rb'
+
+class AddNoteProcessor
+  def get_user_input
+    print "What would you like to add to your list?"
+    gets.chomp
+  end
+
+  def store_input
+    @notes = []
+    user_note = get_user_input()
+    @notes << user_note
+    return @notes
+  end
+
+  class TestAdd < Minitest::Test 
+    def test_add_note
+      processor = AddNoteProcessor.new
+      
+      processor.stub :get_user_input, "test user_note" do
+        assert_equal ["test user_note"], processor.store_input()
+      end
+    end
+  end
+end
