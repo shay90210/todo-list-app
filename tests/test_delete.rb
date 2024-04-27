65dd69fd2d1a33e7bfda36ecc42a758eab66171e
Shannon Bentley <bentley.shannon@yahoo.com>
2024-04-27 18:22:45 +0000

    changed double quotes to single quotes for consistency between tests

--- tests/test_delete.rb
+++ tests/test_delete.rb
@@ -1,7 +1,8 @@
 # tests/test_delete.rb
 
-require "minitest/autorun"
-require_relative "./list.rb"
+require 'minitest/autorun'
+require_relative './list.rb'
 
 class TestDelete < Minitest::Test
+
 end
