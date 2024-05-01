8f80eaec620f4500f50c7e91c3680bfde05c13f6
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-01 03:30:59 +0000

    updated variable name

--- tests/test_delete.rb
+++ tests/test_delete.rb
@@ -4,14 +4,14 @@ require 'minitest/autorun'
 require './list.rb'
 
 class DeleteNoteProcessor
-  def get_user_input
+  def user_choice
     puts "Here is your list. Which note would you like to delete?"
     gets.chomp.to_i
   end
 
   class TestDelete < Minitest::Test
     @notes = ["cook dinner", "wash clothes"]
-    user_delete = get_user_input()
+    user_delete = user_choice()
     @notes.delete_at(index)
   end
 end
