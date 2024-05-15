43f33735708cd113cacc87ea0e7a5d63aba0408b
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-15 03:17:10 +0000

    added formatted_note variable to modify based on the actual argument of the code

--- tests/test_show.rb
+++ tests/test_show.rb
@@ -6,6 +6,8 @@ class TestShowNote < Minitest::Test
     list = List.new
     note = "This is a sample note."
 
+    formatted_note = list.show_note(note)
+
     assert_equal("#{index + 1}. #{note}")
   end
 end
