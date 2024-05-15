70eec121df22f2e05f220e276cd39c02946408b1
Shannon Bentley <bentley.shannon@yahoo.com>
2024-05-15 03:15:07 +0000

    added note variable to define note to correct undefined error

--- tests/test_show.rb
+++ tests/test_show.rb
@@ -4,6 +4,7 @@ require "./list.rb"
 class TestShowNote < Minitest::Test
   def test_show_note
     list = List.new
+    note = "This is a sample note."
 
     assert_equal("#{index + 1}. #{note}")
   end
